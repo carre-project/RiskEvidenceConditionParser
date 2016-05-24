0f30f7f42564d4cc6d67140c080a9ee5fa0dfee0
Nick Portokallidis <portokallidis@gmail.com>
2016-05-25 00:00:00 +0300

    first commit

---
+++ src/jquery.boilerplate.js
@@ -0,0 +1,172 @@
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+var RiskEvidenceConditionParser = {
+	
+    operators: ["!=", "=", ">=", "<=", ">", "<", "OR", "AND"],
+
+    parseAndEvaluateExpression: function(ex) {
+        var arr = ex.split("");
+        for (var i = 0; i < arr.length; i++) {
+
+            if (arr[i] !== " ") return this.parseWithStrings(ex);
+            console.error("ERROR: Expression cannot be empty!");
+            return false;
+        }
+    },
+
+    evaluate: function(condition, vars) {
+		for (var prop in vars ){
+			condition=condition.replaceAll(prop,vars[prop]);
+		}
+		//error control
+		if(condition.indexOf("OB_")>=0) {
+			console.error("==ERROR in OB replacement==")
+			console.error("--Condition:",condition);
+			console.error("--Observable:",
+			condition.substr(condition.indexOf("OB_"),5))
+			return false;
+		}
+        return this.parseAndEvaluateExpression(condition);
+    },
+
+    parseWithStrings: function(s) {
+        var op = this.determineOperatorPrecedenceAndLocation(s);
+        var start = op[0];
+        var left = s.substring(0, start).trim();
+        var right = s.substring(op[1]).trim();
+        var oper = s.substring(start, op[1]).trim();
+        var logType = this.logicalOperatorType(oper);
+
+        console.log("PARSE: Left: \"" + left + "\" Right: \"" + right + "\" Operator: \"" + oper + "\"");
+
+        if (logType == 0) // encounters OR- recurse
+            return this.parseWithStrings(left) || this.parseWithStrings(right);
+        else if (logType == 1) // encounters AND- recurse
+            return this.parseWithStrings(left) && this.parseWithStrings(right);
+        var leftSansParen = this.removeParens(left);
+        var rightSansParen = this.removeParens(right);
+        if (this.isInt(leftSansParen) && this.isInt(rightSansParen))
+            return this.evaluateFloat(parseFloat(leftSansParen), oper, parseFloat(rightSansParen));
+        else return this.evaluateStr(leftSansParen, oper, rightSansParen); // assume they are strings
+    },
+
+    determineOperatorPrecedenceAndLocation: function(s) {
+        s = s.trim();
+        var minParens = 999999999999;
+        var currentMin = null;
+        for (var sampSize = 1; sampSize <= 3; sampSize++) {
+            for (var locInStr = 0; locInStr < (s.length + 1) - sampSize; locInStr++) {
+                var endIndex = locInStr + sampSize;
+                var sub;
+                if ((endIndex < s.length && s[endIndex] == '='))
+                    sub = s.substring(locInStr, ++endIndex).trim();
+                else
+                    sub = s.substring(locInStr, endIndex).trim();
+                if (this.isOperator(sub)) {
+                    // Idea here is to weight logical operators so that they will still be selected over other operators
+                    // when no parens are present
+                    var parens = (this.logicalOperatorType(sub) > -1) ? this.parens(s, locInStr) - 1 : this.parens(s, locInStr);
+                    if (parens <= minParens) {
+                        minParens = parens;
+                        currentMin = [locInStr, endIndex, parens];
+                    }
+                }
+            }
+        }
+        return currentMin;
+    },
+
+    logicalOperatorType: function(op) {
+        if (op.trim()=="OR") {
+            return 0;
+        }
+        else if (op.trim()=="AND") {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    },
+
+    parens: function(s, loc) {
+        var parens = 0;
+        for (var i = 0; i < s.length; i++) {
+            if (s[i] == '(' && i < loc)
+                parens++;
+            if (s[i] == ')' && i >= loc)
+                parens++;
+        }
+        return parens;
+    },
+
+    removeParens: function(s) {
+        s = s.trim();
+        var keep="";
+        s.split("").forEach(function(c){
+            if (!(c == '(') && !(c == ')'))
+                keep+=c;
+        });
+        return keep.toString().trim();
+    },
+
+    isOperator: function(op) {
+        return this.operators.indexOf(op.trim())>=0
+    },
+
+    isInt: function(s) {
+        //is integer or not
+        return parseInt(Number(s)) == s && !isNaN(parseInt(s, 10));
+    },
+
+    evaluateFloat: function(left, op, right) {
+        if (op=="=") {
+            return left == right;
+        }
+        else if (op==">") {
+            return left > right;
+        }
+        else if (op=="<") {
+            return left < right;
+        }
+        else if (op=="<=") {
+            return left <= right;
+        }
+        else if (op==">=") {
+            return left >= right;
+        }
+        else if (op=="!=") {
+            return left != right;
+        }
+        else {
+            console.error("ERROR: Operator type not recognized.");
+            return false;
+        }
+    },
+
+    evaluateStr: function(left, op, right) {
+        if (op=="=") {
+            return left.replaceAll("'", "")==right.replaceAll("'", "");
+        }
+        else if (op=="!=") {
+            return !left.replaceAll("'", "")==right.replaceAll("'", "");
+        }
+        else {
+            console.error("ERROR: Operator type not recognized.");
+            return false;
+        }
+    }
+
+};
+
+
+
+var condition = "( OB_18 = 'stage1' OR OB_18 = 'stage2' OR OB_18 = 'stage3' OR ( OB_30 <= 90 AND OB_30 >= 30 ) ) AND OB_65 = 'yes'"
+var vals = {
+	"OB_18":"stage5",
+	"OB_30":"30.5",
+	"OB_65":"yes"
+};
+console.log(RiskEvidenceConditionParser.evaluate(condition,vals));
